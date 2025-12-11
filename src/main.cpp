24a259d9ae227977fe836a09db1d6adabbdf71f0
cesarMorales228 <andradescesar2024@gmail.com>
2025-12-11 08:50:01 -0400

    Initialize main.cpp with Geode bindings
    Added Geode bindings and a dummy export function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
-// main.cpp mínimo para que el build pase
+#include <Geode/Bindings.hpp>
 
+using namespace geode::prelude;
+
+// De momento solo un símbolo exportado vacío
 extern "C" __declspec(dllexport) void __dummy_geode_entry() {}
